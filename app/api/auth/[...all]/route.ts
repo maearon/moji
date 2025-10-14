4de4940c5ed6a245851c0511c260ad4f11d8757a
maearon <manhng132@gmail.com>
2025-10-14 15:44:55 +0700

    toNextJsHandler fix 405

--- app/api/auth/[...all]/route.ts
+++ app/api/auth/[...all]/route.ts
@@ -2,5 +2,8 @@ export const runtime = "nodejs";
 
 import { auth } from "@/lib/auth";
 import { toNextJsHandler } from "better-auth/next-js";
- 
-export const { GET, POST } = toNextJsHandler(auth);
+
+const handler = toNextJsHandler(auth);
+
+export const GET = handler;
+export const POST = handler;
