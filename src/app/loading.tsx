4d6e644f502f141010fd5198a33273437227fde6
maearon <manhng132@gmail.com>
2025-10-15 15:13:23 +0700

    loading text-white

--- src/app/loading.tsx
+++ src/app/loading.tsx
@@ -3,7 +3,7 @@ import { Loader2 } from "lucide-react";
 export default function Loading() {
   return (
     <div className="flex min-h-svh items-center justify-center p-6">
-      <Loader2 className="text-black dark:text-white size-8 animate-spin" />
+      <Loader2 className="text-white size-8 animate-spin" />
     </div>
   );
 }
